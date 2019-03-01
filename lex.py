a139cf5abf83db96eaa3dc05896abae89003951d
dys-bigwig <45558889+dys-bigwig@users.noreply.github.com>
2019-03-01 23:26:16 +0000

    added NamedTuple(type, value) and string support

--- lex.py
+++ lex.py
@@ -1,4 +1,8 @@
+
+from collections import namedtuple as NamedTuple
+
 def lex(exp):
+    Token = NamedTuple("TOKEN", ("type", "value"))
     tokens = []
     buffer = []
     state = "START"
@@ -8,22 +12,37 @@ def lex(exp):
         char = exp[i]
         if state == "START":
             if char not in ' ()':
-                state = "IDENTIFIER"
+                if char == '"':
+                    state = "STRING"
+                    i += 1
+                else:
+                    state = "IDENTIFIER"
             else:
                 if char == '(':
-                    tokens.append('(')
+                    tokens.append(Token('L_PAREN','('))
                 elif char == ')':
-                    tokens.append(')')
+                    tokens.append(Token('R_PAREN',')'))
                 i += 1
         elif state == "IDENTIFIER":
             if char not in ' ()':
                 buffer.append(char)
                 i += 1
             else:
-                tokens.append("".join(buffer))
+                identifier = "".join(buffer)
+                if identifier.isnumeric():
+                    tokens.append(Token('NUMBER', identifier))
+                else:
+                    tokens.append(Token('IDENTIFIER', identifier))
                 buffer.clear()
                 state = "START"
-
-    print(tokens)
-
-lex(" ((lambda (  x) (* x x ))    7)")
+        elif state == "STRING":
+            if char != '"':
+                buffer.append(char)
+                i += 1
+            else:
+                string = "".join(buffer)
+                tokens.append(Token('STRING', string))
+                buffer.clear()
+                i += 1
+                state = "START"
+    return tokens
