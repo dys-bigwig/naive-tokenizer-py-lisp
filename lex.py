2d74eb75bbc7200d9c781a7d8dffdb9d04aed7bc
dys-bigwig <45558889+dys-bigwig@users.noreply.github.com>
2019-03-05 18:51:48 +0000

    switched to iterator

--- lex.py
+++ lex.py
@@ -1,48 +1,36 @@
+class Lexer:
+    def __init__(self, src):
+        self.src = src
+        self.pos = 0;
+        self.length = len(src)
 
-from collections import namedtuple as NamedTuple
+    def __iter__(self):
+        return self
 
-def lex(exp):
-    Token = NamedTuple("TOKEN", ("type", "value"))
-    tokens = []
-    buffer = []
-    state = "START"
-    i = 0
+    def __next__(self):
+        if self.pos > self.length - 1:
+            raise StopIteration
+        else:
+            ch = self.src[self.pos]
+            if ch == ' ':
+                self.pos += 1
+                return next(self)
+            elif ch in '()':
+                self.pos += 1
+                return ch
+            else:
+                buffer = []
+                while ch not in ' ()':
+                    buffer.append(ch)
+                    self.pos += 1
+                    ch = self.src[self.pos]
+                return "".join(buffer)
 
-    while i < len(exp):
-        char = exp[i]
-        if state == "START":
-            if char not in ' ()':
-                if char == '"':
-                    state = "STRING"
-                    i += 1
-                else:
-                    state = "IDENTIFIER"
-            else:
-                if char == '(':
-                    tokens.append(Token('L_PAREN','('))
-                elif char == ')':
-                    tokens.append(Token('R_PAREN',')'))
-                i += 1
-        elif state == "IDENTIFIER":
-            if char not in ' ()':
-                buffer.append(char)
-                i += 1
-            else:
-                identifier = "".join(buffer)
-                if identifier.isnumeric():
-                    tokens.append(Token('NUMBER', identifier))
-                else:
-                    tokens.append(Token('IDENTIFIER', identifier))
-                buffer.clear()
-                state = "START"
-        elif state == "STRING":
-            if char != '"':
-                buffer.append(char)
-                i += 1
-            else:
-                string = "".join(buffer)
-                tokens.append(Token('STRING', string))
-                buffer.clear()
-                i += 1
-                state = "START"
-    return tokens
+
+def main():
+    l = Lexer("(* (- 5 6) 7)")
+    for tok in l:
+        print("token = " + tok)
+
+if __name__ == '__main__':
+    main()
