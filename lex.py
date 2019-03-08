9ec7d417c56edd89d9b1d666060481ae3ee6ad31
dys-bigwig <45558889+dys-bigwig@users.noreply.github.com>
2019-03-08 18:51:44 +0000

    Rolled a naive peekable iterator

--- lex.py
+++ lex.py
@@ -3,19 +3,31 @@ class Lexer:
         self.src = src
         self.pos = 0;
         self.length = len(src)
+        self.tok = self.advance()        
 
     def __iter__(self):
         return self
+    
+    def peek(self):
+        return self.tok
 
     def __next__(self):
+        retval = self.tok
+        self.tok = self.advance()
+        return retval
+    
+    def advance(self):
         if self.pos > self.length - 1:
             raise StopIteration
         else:
             ch = self.src[self.pos]
-            if ch == ' ':
+            while ch == ' ':
                 self.pos += 1
-                return next(self)
-            elif ch in '()':
+                if self.pos > self.length:
+                    raise StopIteration
+                else:
+                    ch = self.src[self.pos]
+            if ch in '()':
                 self.pos += 1
                 return ch
             else:
@@ -23,14 +35,19 @@ class Lexer:
                 while ch not in ' ()':
                     buffer.append(ch)
                     self.pos += 1
-                    ch = self.src[self.pos]
+                    if self.pos > self.length:
+                        raise StopIteration
+                    else:
+                        ch = self.src[self.pos]
                 return "".join(buffer)
 
 
 def main():
-    l = Lexer("(* (- 5 6) 7)")
+    l = Lexer("(+ (* numA numB) numC))")
+    print("first peek = " + l.peek())
     for tok in l:
         print("token = " + tok)
+        print("peek = " + l.peek())
 
 if __name__ == '__main__':
     main()
